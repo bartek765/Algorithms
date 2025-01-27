59691d4471c1c8b2adaab7b0edf72d987e684c41
Bartek <bartekkedra@onet.pl>
2025-01-27 23:14:38 +0100

    start challenge

---
+++ src/NumberOfCandiesExercise.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public class NumberOfCandiesExercise {
+    public static void main(String[] args) {
+
+    }
+
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        return null;
+    }
+
+}
