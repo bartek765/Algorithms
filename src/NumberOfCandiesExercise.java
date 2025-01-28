052f9f9ec1e31bf38ffb6e90004d42403a4949be
Bartosz Kędra <bartekkedra@onet.pl>
2025-01-28 12:29:20 +0100

    Update NumberOfCandiesExercise.java

--- src/NumberOfCandiesExercise.java
+++ src/NumberOfCandiesExercise.java
@@ -1,12 +1,10 @@
-import java.util.List;
-
-public class NumberOfCandiesExercise {
-    public static void main(String[] args) {
-
-    }
-
+class Solution {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
-        return null;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < candies.length; i++) {
+            if (candies[i] > max) {
+                max = candies[i];
+            }
+        }
     }
-
 }
