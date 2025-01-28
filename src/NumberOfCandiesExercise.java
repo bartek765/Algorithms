e1f796b93f8ee630ffa47121a5dc585b9b546d97
Bartosz Kędra <bartekkedra@onet.pl>
2025-01-29 00:07:37 +0100

    FinalUpdate NumberOfCandiesExercise.java

--- src/NumberOfCandiesExercise.java
+++ src/NumberOfCandiesExercise.java
@@ -6,5 +6,15 @@ class Solution {
                 max = candies[i];
             }
         }
+        List<Boolean> result = new ArrayList<>();
+        for (int i = 0; i < candies.length; i++) {
+            if (candies[i] + extraCandies >= max) {
+                result.add(true);
+            }
+            else {
+                result.add(false);
+            }
+        }
+        return result;
     }
 }
