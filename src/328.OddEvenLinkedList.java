d9a27e552e9530e85d9f9d462631775dc7be230b
Bartosz Kędra <bartekkedra@onet.pl>
2025-04-27 00:28:57 +0200

    Create 328.OddEvenLinkedList.java

---
+++ src/328.OddEvenLinkedList.java
@@ -0,0 +1,17 @@
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode a = head;
+        ListNode b = head.next, c = b;
+        while (b != null && b.next != null) {
+            a.next = b.next;
+            a = a.next;
+            b.next = a.next;
+            b = b.next;
+        }
+        a.next = c;
+        return head;
+    }
+}
